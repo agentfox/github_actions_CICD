14b67b18f5b55977990d2103cef36e405c0a2bbc
CUONGWRU <cuonglcq23@wru.vn>
2023-10-15 07:40:08 +0700

    new commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by  me and      {' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
